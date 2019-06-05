0cc922b92e9b7e37e395b35f20a401692943023a
python-code-level <python-training@outlook.com>
2019-06-05 13:33:20 +0100

    Create admin.sh

---
+++ admin.sh
@@ -0,0 +1,22 @@
+
+
+
+# show dir content with human readable file size
+
+ls -lh
+
+
+# show dir content with hidden files and folders
+
+ls -al
+
+# show dir content - sorted by most recently modified
+
+ls -lt
+
+# show dir content - sort by file size
+
+ls -Sl
+
+
+
